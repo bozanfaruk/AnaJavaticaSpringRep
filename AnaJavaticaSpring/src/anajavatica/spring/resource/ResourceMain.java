27fd6de6d2453e887d9742312ac5f86f3f12167d
bozanfaruk <bozanfaruk@gmail.com>
2016-08-03 10:31:53 +0300

    Resource interface commit.

---
+++ AnaJavaticaSpring/src/anajavatica/spring/resource/ResourceMain.java
@@ -0,0 +1,42 @@
+package anajavatica.spring.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+
+public class ResourceMain {
+
+	public static void main(String[] args) throws IOException {
+
+		ResourceMain.executeUrlResource();
+		ResourceMain.executeFileResource();
+
+	}
+
+	private static void executeUrlResource() throws IOException {
+		Resource urlResoource = new UrlResource("http://www.farukbozan.com/feed");
+		InputStream is = urlResoource.getInputStream();
+		if (urlResoource.exists()) {
+			if (is.available() <= 0)
+				System.out.println("Nothing to read");
+			else
+				System.out.println(urlResoource.getDescription());
+			is.close();
+		}
+	}
+
+	private static void executeFileResource() throws IOException {
+		Resource fileResource = new FileSystemResource("fileResource.txt");
+		InputStream is = fileResource.getInputStream();
+		if (fileResource.exists()) {
+			byte[] cache = new byte[50];
+			is.read(cache);
+			System.out.println(new String(cache));
+			is.close();
+		}
+	}
+
+}
