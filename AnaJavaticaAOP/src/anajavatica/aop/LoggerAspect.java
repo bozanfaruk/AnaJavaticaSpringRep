b4a900e1ef7d2e0ce245f0e578c8f15da4740df5
bozanfaruk <bozanfaruk@gmail.com>
2016-08-16 16:47:56 +0300

    Pointcut commit.

--- AnaJavaticaAOP/src/anajavatica/aop/LoggerAspect.java
+++ AnaJavaticaAOP/src/anajavatica/aop/LoggerAspect.java
@@ -2,6 +2,7 @@ package anajavatica.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggerAspect {
@@ -9,6 +10,15 @@ public class LoggerAspect {
 	@Before("execution(public int anajavatica.aop.Car.getHP(..))")
 	public void loggingBeforeHP() {
 		log("Before HP info...");
+	}
+
+	@Before("execution(public * get*(..))")
+	public void loggingBeforeAnyGet() {
+		log("A Get method called...");
+	}
+
+	@Pointcut("execution(public * get*(..))")
+	public void loggingPointcutAnyGet() {
 	}
 
 	private void log(String loggingInfo) {
